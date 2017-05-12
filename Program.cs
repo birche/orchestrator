734ba1952f7a6e872912573614fa9e3c99cbd8e6
Jens Nordenbro <jnb@beijer.se>
2017-05-12 16:27:12 +0200

    starting beijer-service although hardcoded

--- Program.cs
+++ Program.cs
@@ -10,20 +10,29 @@ namespace process_tracker
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Process pInfo = Process.Start("top");
-            Process pInfo2 = Process.Start("top");
+            ProcessStartInfo startInfor = new ProcessStartInfo("/var/storage/repo/dotnet/dotnet", "AutoTransfer.dll");
+            startInfor.WorkingDirectory = "/var/storage/repo/autoupdateserver/netcoreapp2.0/linux-arm/publish";
+            Process pInfo = Process.Start(startInfor);
 
             List<Task> list = new List<Task>();
-
-            Process []processes ={pInfo, pInfo2};
+            int count = 0;
+            Process []processes ={pInfo};
             foreach (var process in processes) {
                 Task t = Task.Run( async () => {
                     string moduleName = process.MainModule.ModuleName;
                     int id = process.Id;
 
+                
                     while(true)
                     {
-                        const double MB = 1024.0*1024.0;
+                        if (count++ == 20)
+                        {
+                            process.Kill();
+                        }
+
+                        Console.WriteLine("Count =" + count);
+
+                        const double MB = 1; //1024.0*1024.0;
                         Console.WriteLine(moduleName + " with pid " + id + " is alive. Private memory: " + process.PrivateMemorySize64/MB + "MB, Peak paged memory: " + process.PeakPagedMemorySize64/MB+ "MB");   
                         if(process.HasExited)
                         {
